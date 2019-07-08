8cda802788f80f36b6c0db8ccd6f411d95bd5243
shakib-digitruck <shakib@digitruck.com.bd>
2019-07-08 16:30:08 +0600

    Second Commit

--- NodeJS/models/todo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-var Todo = mongoose.model('Todo', {
-    name: { type: String },
-    duration: { type: Number },
-    status: { type: String }
-});
-
-module.exports = { Todo };
